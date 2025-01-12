8926ca2cc82911e186b96e97ed25cb9421dfc533
Deepak karki <ideepakkarki@gmail.com>
2025-01-12 16:58:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//your JS code here. If required.
 let student = {
   name: "deepak",
-  getkeys: function () {
-    return Object.keys(this);
-  },
 };
 
-console.log(student.getKeys())
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
